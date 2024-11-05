5e40af9fc48d11e3fc178f0c40b48e4213a48321
alicemariaarrica <alicemariaarrica@gmail.com>
2024-11-05 15:30:40 +0100

    array-postcontrollers-commit

---
+++ app.js
@@ -0,0 +1,15 @@
+//Esercizio parte 1
+//Usando l'array dei post fornito con le relative immagini, creare un file di routing (routers/posts.js) che conterrà le rotte necessario per l'entità post.
+//Tutte le risposte saranno in json.
+//All'interno creare le seguenti rotte:
+// / - index: ritornerà un html con una ul che stamperà la lista dei post
+// /:slug - show: tramite il parametro dinamico che rappresenta lo slug del post, ritornerà un json con i dati del post
+//Scrivere tutte le funzioni delle rotte nel controller dedicato
+//Registrare il router dentro app.js con il prefisso posts/.
+
+//Esercizio parte 2
+//Per il nostro blog, concentriamoci sul creare una rotta:
+//la rotta dovrá rispondere al verbo POST e delegare un metodo store del controller dei posts per effettuare le operazioni di creazione della risorsa.
+//Questa dovrà riceve i dati in formato json e dovrà restituire l'elenco dei posts in formato json incluso il nuovo elemento appena creato
+//Tutte le funzioni delle rotte dovranno essere scritte nel controller dedicato.
+//Testare le rotte tramite Postman.
---
+++ postControllers.js
@@ -0,0 +1,40 @@
+
+const posts = [
+    {
+      title: "La Ricetta del Ciambellone Perfetto",
+      slug: "ricetta-ciambellone",
+      content: "Scopri come preparare un ciambellone soffice e gustoso, ideale per la colazione e la merenda.",
+      image: "/imgs/ciambellone.jpg",
+      tags: ["dolci", "colazione", "ciambellone"]
+    },
+    {
+      title: "Cracker alla Barbabietola: Snack Salutare",
+      slug: "cracker-barbabietola",
+      content: "I cracker alla barbabietola sono un'alternativa sana e colorata per uno snack croccante e saporito.",
+      image: "/imgs/cracker.barbabietola.jpg",
+      tags: ["snack", "salutare", "barbabietola"]
+    },
+    {
+      title: "Pane Fritto Dolce: Tradizione e Gusto",
+      slug: "pane-fritto-dolce",
+      content: "Il pane fritto dolce è una ricetta tradizionale che conquista grandi e piccoli, ideale per un dessert semplice.",
+      image: "/imgs/pane_fritto_dolce.jpg",
+      tags: ["dolci", "tradizionale", "dessert"]
+    },
+    {
+      title: "Pasta alla Barbabietola: Un Piatto Colorato",
+      slug: "pasta-barbabietola",
+      content: "La pasta alla barbabietola è un primo piatto colorato e saporito, perfetto per stupire i commensali.",
+      image: "/imgs/pasta_barbabietola.jpg",
+      tags: ["primi piatti", "vegetariano", "barbabietola"]
+    },
+    {
+      title: "Torta Paesana: Dolci Ricordi di Infanzia",
+      slug: "torta-paesana",
+      content: "La torta paesana è un dolce tradizionale, tipico delle feste, dal gusto intenso e avvolgente.",
+      image: "/imgs/torta_paesana.jpg",
+      tags: ["dolci", "tradizionale", "torta"]
+    }
+];
+
+module.exports = posts;
