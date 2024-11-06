8e5cbf3ecb4ba8305dabd82742fc3162f349782b
alicemariaarrica <alicemariaarrica@gmail.com>
2024-11-06 14:09:00 +0100

    part2-import-array-in-app.js

--- app.js
+++ app.js
@@ -27,6 +27,10 @@
 const express = require('express')
 const app = express()
 
+
+const menu = require('./db/menu.js')
+
+
 app.listen(3000, () => {
   console.log("Server started on port 3000")
 });
