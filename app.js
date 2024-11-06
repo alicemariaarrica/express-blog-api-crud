d0935645b870ded1c8756b3cb103d78aef5ba165
alicemariaarrica <alicemariaarrica@gmail.com>
2024-11-06 15:01:03 +0100

    import-controller-file-commit

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.get('/cibi/:id', (req, res) => {
 
 
 
-const PizzaController = require('./controllers/PizzaController');
+const postsController = require('./controllers/postsControllers');
 
 app.listen(3000, () => {
   console.log("Server started on port 3000")
